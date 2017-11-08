f098349037568be37bfa309bf3b3cea90fea0075
leeikjeun <dldlrwjs12@gmail.com>
2017-11-08 19:59:56 +0900

    reset setting

--- mainWeb/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, UnknowNameBoard, ProffessorData, Proffessorcontent
-# Register your models here.
-
-admin.site.register(Profile)
-admin.site.register(UnknowNameBoard)
-admin.site.register(ProffessorData)
-admin.site.register(Proffessorcontent)
--- mainWeb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainwebConfig(AppConfig):
-    name = 'mainWeb'
