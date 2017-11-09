61a109be83278c8505643c4e3af6fb6d767e211c
llkjop24 <llkjop24@gmail.com>
2017-11-09 23:14:38 +0900

    profe_info, view 경로 설정

--- conf/mainWeb/views.py
+++ conf/mainWeb/views.py
@@ -47,7 +47,7 @@ def unknown_post_detail(request, pk):
     return render(request, 'mainWeb/Unknown_post/Unknown_post_detail.html', {})
 
 def profe_info(request):
-    return render(request, 'mainWeb/Profe_info/Profe_info.html', {})
+    return render(request, 'mainWeb/Profe_intro/Profe_info.html', {})
 
 def ggul_tip(request):
     return render(request, 'mainWeb/Profe_info/GGul_tip/GGul_tip.html', {})
